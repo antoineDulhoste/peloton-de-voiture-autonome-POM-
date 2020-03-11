9831bf83bf98d547199644a2a8baebafd08f820d
BuildTools <unconfigured@null.spigotmc.org>
2020-03-11 14:43:12 +0100

    fixed display. img still bugged

--- src/main.cpp
+++ src/main.cpp
@@ -10,30 +10,40 @@ using namespace std;
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+SDL_Window* window = NULL;  //initialize to null to setup renderer and textures before first display
+SDL_Renderer *renderer = NULL;
+
 int main(int argc, char* args[]) {
 
   srand(110);
   Map m(10, 100,100);
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());return 1;}
-  if (IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG) < 0) {fprintf(stderr, "could not initialize sdl2_image: %s\n", SDL_GetError());return 1;}
-
-  SDL_Window* window = NULL;  //initialize to null to setup renderer and textures before first display
-  SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  SDL_Texture* texBackground = IMG_LoadTexture(renderer,"../data/grass.jpg");  //Load in GPU
-  SDL_Rect rectBackground = { 100, 100, 100, 100 };
-  SDL_Event event;
+  if (IMG_Init(IMG_INIT_JPG) < 0) {fprintf(stderr, "could not initialize sdl2_image: %s\n", SDL_GetError());return 1;}
 
   window = SDL_CreateWindow("POM",
                 			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                 			    SCREEN_WIDTH, SCREEN_HEIGHT,
                 			    SDL_WINDOW_SHOWN);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  SDL_Texture* texBackground = IMG_LoadTexture(renderer,"../data/grass.JPG");  //Load in GPU
+  SDL_Rect rectBackground = { 5, 5, 500, 500 };
+
+  SDL_Event event;
 
   if (window == NULL) {fprintf(stderr, "could not create window: %s\n", SDL_GetError());return 1;}
-  SDL_SetRenderDrawColor(renderer,255,0,0,255);
+  SDL_SetRenderDrawColor(renderer,0,0,0,255);
+
   bool quit = false;
   while(!quit){
     SDL_PollEvent(&event);  // Récupération des actions de l'utilisateur
+
+    SDL_RenderCopy(renderer, texBackground, NULL, &rectBackground);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    //printf("SDL : %s\n", SDL_GetError());
+
     switch(event.type){
         case SDL_QUIT: // Clic sur la croix
             quit=1;
@@ -44,10 +54,6 @@ int main(int argc, char* args[]) {
             }
             break;
     }
-
-    SDL_RenderCopy(renderer, texBackground, NULL, &rectBackground);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
   }
   SDL_DestroyTexture(texBackground);
   SDL_DestroyRenderer(renderer);
