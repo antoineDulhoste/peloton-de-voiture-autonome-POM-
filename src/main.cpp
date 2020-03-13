d3f616166de67f804748bd4cde22d65df8529e19
BuildTools <unconfigured@null.spigotmc.org>
2020-03-13 13:27:30 +0100

    fixed main display

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,18 @@ using namespace std;
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+#define CIRCLE_SIZE 20
 
 SDL_Window* window = NULL;  //initialize to null to setup renderer and textures before first display
 SDL_Renderer *renderer = NULL;
 
 int main(int argc, char* args[]) {
 
-  srand(110);
+  srand(93);    //seed random
   Map m(10, 100,100);
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());return 1;}
-  if (IMG_Init(IMG_INIT_JPG) < 0) {fprintf(stderr, "could not initialize sdl2_image: %s\n", SDL_GetError());return 1;}
+  if (IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG) < 0) {fprintf(stderr, "could not initialize sdl2_image: %s\n", SDL_GetError());return 1;}
 
   window = SDL_CreateWindow("POM",
                 			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -28,22 +29,30 @@ int main(int argc, char* args[]) {
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
   SDL_Texture* texBackground = IMG_LoadTexture(renderer,"../data/grass.JPG");  //Load in GPU
-  SDL_Rect rectBackground = { 5, 5, 500, 500 };
+  SDL_Texture* texCircle = IMG_LoadTexture(renderer,"../data/circle.PNG");  //Load in GPU
+  SDL_Rect rectBackground = { 0, 0, SCREEN_HEIGHT, SCREEN_HEIGHT };
+
 
   SDL_Event event;
 
   if (window == NULL) {fprintf(stderr, "could not create window: %s\n", SDL_GetError());return 1;}
-  SDL_SetRenderDrawColor(renderer,0,0,0,255);
 
   bool quit = false;
   while(!quit){
     SDL_PollEvent(&event);  // Récupération des actions de l'utilisateur
 
-    SDL_RenderCopy(renderer, texBackground, NULL, &rectBackground);
+    SDL_SetRenderDrawColor(renderer,0,0,0,255);
     SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    //printf("SDL : %s\n", SDL_GetError());
+    SDL_RenderCopy(renderer, texBackground, NULL, &rectBackground);
 
+    SDL_Rect rectCircle;
+    for(unsigned i=0; i<m.getMap().size(); i++){
+      rectCircle = { m.getMap().at(i).getX()*SCREEN_HEIGHT/100-CIRCLE_SIZE/2,
+                     m.getMap().at(i).getY()*SCREEN_HEIGHT/100-CIRCLE_SIZE/2,
+                     CIRCLE_SIZE, CIRCLE_SIZE};
+      SDL_RenderCopy(renderer, texCircle, NULL, &rectCircle);
+    }
+    SDL_RenderPresent(renderer);
     switch(event.type){
         case SDL_QUIT: // Clic sur la croix
             quit=1;
@@ -55,6 +64,7 @@ int main(int argc, char* args[]) {
             break;
     }
   }
+  SDL_DestroyTexture(texCircle);
   SDL_DestroyTexture(texBackground);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
